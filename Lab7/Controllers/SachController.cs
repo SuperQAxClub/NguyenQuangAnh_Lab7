0a3fc11e38416d7136dfc35c8c4e97e5f87f8b02
SuperQAxClub <nguyenquanganh38@gmail.com>
2020-06-25 10:49:20 +0700

    Completed Web API demo

---
+++ Lab7/Controllers/SachController.cs
@@ -0,0 +1,34 @@
+﻿using Lab7.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace Lab7.Controllers
+{
+    public class SachController : ApiController
+    {
+        Sach[] sachs = new Sach[]
+        {
+            new Sach {Id = 1, Title = "Some random books", AuthorName = "Some nigga author", Price = 1, Content="Some Ricardo Milos story" },
+            new Sach {Id = 2, Title = "Some more random books", AuthorName = "Some other nigga author", Price = 1, Content="Some more Ricardo Milos story" }
+        };
+
+        public IEnumerable<Sach> GetAll()
+        {
+            return sachs;
+        }
+
+        public IHttpActionResult GetSach(int id)
+        {
+            var sach = sachs.FirstOrDefault((p) => p.Id == id);
+            if(sach == null)
+            {
+                return NotFound();
+            }
+            return Ok(sach);
+        }
+    }
+}
